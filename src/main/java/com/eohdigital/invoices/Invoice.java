57f70c32002aa6d002d23ae289fca9e8d4b2b025
f5038790 <matsobane.khwianana@fnb.co.za>
2018-07-06 13:07:37 +0200

    Remove unused import statement

--- src/main/java/com/eohdigital/invoices/Invoice.java
+++ src/main/java/com/eohdigital/invoices/Invoice.java
@@ -2,7 +2,6 @@ package com.eohdigital.invoices;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Date;
